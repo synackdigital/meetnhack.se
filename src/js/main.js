42ca0ca0f96abe16141a5d46483f340464fd93bd
frebro <frebro@gmail.com>
2013-04-03 00:02:32 +0200

    tipsy on speakers

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('#navigation').localScroll({
       offset: -60
     });
+    $('[data-toggle="tipsy"]').tipsy({
+      fade: true,
+      gravity: 's',
+      delayIn: 200,
+      delayOut: 800
+    });
 });
